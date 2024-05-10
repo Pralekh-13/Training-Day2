225c4aedcc2787e09e74d6cef5b0bc3aebb0aff3
Pralekh <pralekhvrambhad@gmail.com>
2024-05-10 11:54:29 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import Model.Employee;
+
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
-        System.out.println("hello");
+        //System.out.println("hello");
+
+Employee employee=new Employee("neha","marketing head");
+        employee.display("kavish","Sr Sd");//for NonStatic method
+//        Employee.display("kavish","Sr Sd");// For Static method
+
     }
 }
---
+++ src/Model/Employee.java
@@ -0,0 +1,39 @@
+package Model;
+
+public class Employee {
+
+    private String eName;
+    private String position;
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Employee(String eName, String position) {
+        this.eName = eName;
+        this.position = position;
+    }
+
+    public Employee() {
+    }
+
+
+//    public static void display(String eName, String position){
+//        System.out.println(eName+" "+position);
+//    }
+    public void display(String eName, String position){
+        System.out.println(eName+" "+position);
+    }
+}
