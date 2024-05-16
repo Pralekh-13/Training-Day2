08c23edbe36ce082f9c53a715377b765d44e778c
Pralekh <pralekhvrambhad@gmail.com>
2024-05-16 11:28:15 +0530

    dateFormatter changed

--- src/Collection/ArraList1.java
+++ src/Collection/ArraList1.java
@@ -18,19 +18,22 @@ public class ArraList1 {
 
         ArrayList<Employee> arrayList1=new ArrayList<>();
        //Employee employee = null;
-       Employee employee1=new Employee(1,"gfhgf","fghfhg");
+       Employee employee1=new Employee(2,"gfhgf","fghfhg");
        Employee employee2=new Employee(1,"gfhgf","fghfhg");
         arrayList1.add(employee1 );
 
        arrayList1.add(employee2);
-       arrayList1.remove(employee1);
-       System.out.println(arrayList1);
-//        Collections.sort(arrayList1,employee1.compare(employee2));
+//       arrayList1.remove(employee1);
+//       System.out.println(arrayList1);
+        Collections.sort(arrayList1,new EmployeeIdComparator());
+        for(Employee employee:arrayList1){
+            System.out.println(employee);
+        }
 
-        Set<Employee> set = new HashSet<>();
-        set.add(employee1);
-        set.add(employee2);
-        System.out.println("set "+ set);
+//        Set<Employee> set = new HashSet<>();
+//        set.add(employee1);
+//        set.add(employee2);
+//        System.out.println("set "+ set);
         //Set<Integer>set1 =new HashSet<>();
 
 
